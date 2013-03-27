d9386e323c7d7a7486f75d2d3d752569c8ac4a75
Conor O'Callaghan <conor_oc@ymail.com>
2013-03-27 22:52:25 +0000

    Prototype

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-
+    @resources = Resource.all(params[:user_id])
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
